d3c687f3a67bae5279b4212e7c0a7a7311860364
Yoann <yoann.catala@imerir.com>
2016-05-24 22:56:38 +0200

    test-products

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-# -*- coding: utf-8 -*-
-from flask import Flask
+import json
+from flask import Flask, make_response, request
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-  return "Hello world!"
-
+products = ["Pear", "Apple"]
+@app.route("/products/")
+def products():
+    resp = make_response(json.dumps(products), 200)
+    resp.headers['Content-Type'] = 'application/json'
+    return  resp
+@app.route("/products/", methods=["POST"])
+def products_add():
+    global products
+    prod = request.get_json()
+    products.append(prod)
+    resp = make_response("True", 201)
+    resp.headers['Content-Type'] = 'application/json'
+    resp.headers['Location'] = '/products/%d' % (len(products) - 1)
+    return  resp
 if __name__ == "__main__":
-  app.debug=True
-  app.run()
+    app.run()
